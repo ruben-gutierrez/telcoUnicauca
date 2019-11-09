3d3213ef053a800789044395ef377572c5295d26
ruben-gutierrez <rubengutierrez@unicauca.edu.co>
2019-11-08 20:15:11 -0500

    frontend: Template ok

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -3,10 +3,24 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { ProyectsComponent } from './components/proyects/proyects.component';
+import { AboutComponent } from './components/about/about.component';
+import { ContactComponent } from './components/contact/contact.component';
+import { ConsoleComponent } from './components/console/console.component';
+import { NavbarComponent } from './components/shared/navbar/navbar.component';
+import { FooterComponent } from './components/shared/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    ProyectsComponent,
+    AboutComponent,
+    ContactComponent,
+    ConsoleComponent,
+    NavbarComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
---
+++ frontend/src/app/components/proyects/proyects.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-proyects',
+  templateUrl: './proyects.component.html',
+  styles: []
+})
+export class ProyectsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
